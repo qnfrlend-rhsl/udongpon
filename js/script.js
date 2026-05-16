07b1e84333e5416ade4a50a42fb99b81a1c04593
너비 초이 <your_email@example.com>
2026-05-16 19:08:46 +0900

    update

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ let currentCategory = "전체";
 
 // 🔥 배지 크기 (여기서 조절)
 let badgeSize = 20;
-let badgeOffsetX = -5;
-let badgeOffsetY = -10;
+let badgeOffsetX = 2;
+let badgeOffsetY = -8;
 
 // 📍 지역 좌표
 const dongCoords = {
