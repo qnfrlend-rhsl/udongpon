7d0e803fea0691ccb5ab00a6a3edd761d049c521
너비 초이 <your_email@example.com>
2026-05-16 18:36:29 +0900

    update

--- js/script.js
+++ js/script.js
@@ -130,7 +130,7 @@ function renderMarkers(storesData) {
         justify-content:center;
         box-shadow:0 1px 3px rgba(0,0,0,0.25);
       ">
-      🔥
+      💸
     </div>
 
     <div class="badge-tooltip">
