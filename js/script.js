f86d885c0ad86396b2174414f13e0a72936ed8db
너비 초이 <your_email@example.com>
2026-05-16 20:55:45 +0900

    update

--- js/script.js
+++ js/script.js
@@ -153,7 +153,7 @@ function renderMarkers(storesData) {
 
     marker.bindTooltip(store.storeName, {
     direction: "bottom",
-    offset: [0, 25],
+    offset: [0, 20],
     permanent: false,
     sticky: true
     });
