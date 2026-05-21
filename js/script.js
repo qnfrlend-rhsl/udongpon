3e5e9cffe3aafd6d8d4f5f391bd236508e4bcfb4
너비 초이 <your_email@example.com>
2026-05-21 15:23:42 +0900

    update

--- js/script.js
+++ js/script.js
@@ -186,7 +186,7 @@ function renderMarkers(storesData) {
              style="
                position:absolute;
                top:${badgeOffsetY - badgeSize * 0.4}px;
-               left:${badgeOffsetX + 5}px;
+               left:${badgeOffsetX + 12}px;
              ">
 
           <div class="badge-icon"
