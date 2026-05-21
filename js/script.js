67522d587a20df611c000b822b542dff787bc10c
너비 초이 <your_email@example.com>
2026-05-21 14:35:33 +0900

    update

--- js/script.js
+++ js/script.js
@@ -173,9 +173,9 @@ function renderMarkers(storesData) {
       <!-- 🏪 매장 이름 (🔥 이것만 추가) -->
       <div style="
         font-size:${status === "active" ? 11 : 9}px;
-        color:${status === "active" ? "#e21802" : "#9b9999"};
+        color:${status === "active" ? "#8f0e00" : "#636363"};
         white-space:nowrap;
-        margin-top:2px;
+        margin-top:-1px;
       ">
         ${store.storeName}
       </div>
