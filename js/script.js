111aaeffb4356aa165d7ebdc135832024fe2b040
불기둥-고니 <ksk710501@gmail.com>
2026-07-21 12:20:16 +0900

    merge remote

--- js/script.js
+++ js/script.js
@@ -110,19 +110,12 @@ function updateStats() {
   );
 
   let newsText =
-`<span style="color:#ffd700;font-weight:bold; font-size:13px; display:inline-flex; align-items:center; gap:5px;">
-  <span style="font-size:12px;">📢</span>
-  현재 이벤트매장 ${eventStores.length}곳 운영중 [ 문의전화 : 010-8429-5368 ]
-</span>
-<span style="margin-left:8px; font-size:10px; color:#ff0000; opacity:0.8;">▶</span> `;
+  `📢 현재 이벤트매장 ${eventStores.length}곳 운영중 [ 문의전화 : 010-8429-5368 ] ▶ `;
 
-  eventStores.forEach(store => {
-  newsText += `
-  <span style="color:#ffffff;">
-    ${store.dong} ${store.storeName}
-  </span>
-  <span style="display:inline-block; margin-left:25px; font-size:10px; color:#ff0000;">▶</span> `;
-  });
+eventStores.forEach(store => {
+  newsText +=
+    `${store.dong} ${store.storeName} ▶ `;
+});
 
   document.getElementById("newsTrack").innerHTML =
   newsText;
