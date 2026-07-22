20a40b57858be104ec209b58029cef533ff413b5
불기둥-고니 <ksk710501@gmail.com>
2026-07-22 23:03:45 +0900

    merge remote

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.addEventListener("DOMContentLoaded", () => {
   map = L.map('map', {
     minZoom: 6,
     maxZoom: 19
-  }).setView([37.8822121, 127.7480846], 16.4);
+  }).setView([37.8828714, 127.7465298], 16.6);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors',
